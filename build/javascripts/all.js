3e2b04612b49ad10ac7140cc49ee6ef47b4d46ef
bhurlow <bhurlow@gmail.com>
2013-02-18 12:00:57 -0600

    added dl link

--- build/javascripts/all.js
+++ build/javascripts/all.js
@@ -1,3 +1,44 @@
+/*
+
+Script to manage email submissions
+
+*/
+
+
+
+
+$(function($) {
+
+	var db_url = 'http://localhost:5984/sandbox';
+
+	var input = $('.input_email');
+	var btn_submit = $('.email_submit');
+	btn_submit.click(function() {
+		var val = input.val();
+		var isEmail = validateEmail(val)
+		if(isEmail) sendEmail(val)
+		input.val('thanks!');
+	})
+
+	function validateEmail(email) {
+		var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		return re.test(email);
+	}
+
+	function sendEmail(email_string) {
+
+		$.ajax({
+			url: db_url,
+			method: "POST",
+			data: {
+				"item": email_string
+			}
+		});
+
+	}
+
+	})
+;
 
 var $thing = $('.thing')
 
@@ -1334,3 +1375,6 @@ So the key logic in this
   })();
 
 }).call(this);
+
+console.log('sup')
+;
---
+++ source/javascripts/email.js
@@ -0,0 +1,39 @@
+/*
+
+Script to manage email submissions
+
+*/
+
+
+
+$(function($) {
+
+	var db_url = 'http://localhost:5984/sandbox';
+
+	var input = $('.input_email');
+	var btn_submit = $('.email_submit');
+	btn_submit.click(function() {
+		var val = input.val();
+		var isEmail = validateEmail(val)
+		if(isEmail) sendEmail(val)
+		input.val('thanks!');
+	})
+
+	function validateEmail(email) {
+		var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		return re.test(email);
+	}
+
+	function sendEmail(email_string) {
+
+		$.ajax({
+			url: db_url,
+			method: "POST",
+			data: {
+				"item": email_string
+			}
+		});
+
+	}
+
+	})
