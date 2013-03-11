e55102ce9004c3a4bd750d113f27a9cfb0e42989
bhurlow <bhurlow@gmail.com>
2013-03-11 02:06:21 -0500

    added simple epk

--- config.rb
+++ config.rb
@@ -55,6 +55,8 @@ set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
 
+activate :directory_indexes
+
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
@@ -67,7 +69,7 @@ configure :build do
   # activate :cache_buster
 
   # Use relative URLs
-  activate :relative_assets
+  # activate :relative_assets
 
   # Compress PNGs after build
   # First: gem install middleman-smusher
