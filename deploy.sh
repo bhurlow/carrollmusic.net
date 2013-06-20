6c0de5953ad60d4f613f41590cca2bd53b57fbb8
Brian Hurlow <bhurlow@gmail.com>
2013-06-20 14:08:47 -0500

    no need for www

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,3 @@
 rsync -vr ./build/* root@foam.superflyer.in:/root/websites/carrollmusic.net --exclude-from exclude.txt
-rsync -vr ./build/* root@foam.superflyer.in:/root/websites/www.carrollmusic.net --exclude-from exclude.txt
 
 
