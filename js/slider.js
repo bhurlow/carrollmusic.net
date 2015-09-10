146f271b8bdd23b9d5fe3a10955f1d3eae423330
bhurlow <bhurlow@gmail.com>
2015-09-09 23:58:40 -0400

    mobile setup

--- js/slider.js
+++ js/slider.js
@@ -8,12 +8,11 @@ function slider() {
   //   $('#slider').css('background-position-y', '-' + window.scrollY + 'px') 
   // })
 
-  var posY = 0;
-  setInterval(function() {
-    posY += 50
-    $('#slider').css('background-position-y', '-' + posY + 'px') 
-    // $('#slider').css('background-position-x', '-' + posY + 'px') 
-  }, 2000)
+//   var posY = 0;
+//   setInterval(function() {
+//     posY += 50
+//     $('#banner').css('background-position-y', '-' + posY + 'px') 
+//   }, 2000)
 
 }
 
